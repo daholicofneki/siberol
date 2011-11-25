398654351a47fb0caacc5a0fc4f798fef76d82d7
daholicofneki <neki.arismi@gmail.com>
2011-11-25 17:14:37 +0700

    change layout

---
+++ index.php
@@ -0,0 +1,53 @@
+<html>
+	<head>
+		<title></title>
+		<link href="public/css/bootstrap.min.css" media="screen" rel="stylesheet" type="text/css" />
+		<link href="public/css/custom.css" media="screen" rel="stylesheet" type="text/css" />
+		<script src="public/js/jquery-1.7.min.js" type="text/javascript"></script>
+	</head>
+	<body>
+<!--
+<div class="topbar">
+	<div class="fill">
+		<div class="container">
+			<a class="brand" href="#">HEALTH INFO</a>
+			<ul class="nav">
+				<li class="active"><a href="#">Home</a></li>
+				<li><a href="#about">About</a></li>
+				<li><a href="#contact">Contact</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+-->
+<div class="container">
+
+	<!-- Main hero unit for a primary marketing message or call to action -->
+	<div class="hero-unit">
+	<h1>Hello, world!</h1>
+	<p>Vestibulum id ligula porta felis euismod semper. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+	<p><a class="btn primary large">Learn more &raquo;</a></p>
+	</div>
+	
+	<!-- Example row of columns -->
+	<div class="row">
+	<div class="span6">
+	<h2>Categories</h2>
+	<p>Etiam porta sem malesuada magna mollis euismod. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+	<p><a class="btn" href="#">View details &raquo;</a></p>
+	</div>
+	<div class="span10">
+	<h2>Recent articles</h2>
+	<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+	<p><a class="btn" href="#">View details &raquo;</a></p>
+	</div>
+	</div>
+	
+	<footer>
+	<p>&copy; Neki, Nurvina - Server Side Scripting 2011</p>
+	</footer>
+
+</div>
+
+	</body>
+</html>
