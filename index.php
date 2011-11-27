ff6c87a7366fe26f5c06ef3e3ab4068a160af1fb
unknown <daholicofneki@daholicofneki.(none)>
2011-11-27 17:49:08 +0700

    ubah halaman utama

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
-require_once ('library/database.php');
-if ($_POST)
-{
-	$db = new DB();
-	$db::query ('INSERT INTO berita (judul,isi,id_wartawan) VALUES ("'.$_POST['_judul'].'","'.$_POST['_isi'].'",1)');
-}
+require_once ('library/config.php');
+$data = $DB->get_all('SELECT * FROM berita ORDER BY tanggal_publikasi LIMIT 5');
 ?>
 <html>
 	<head>
@@ -26,20 +22,6 @@ if ($_POST)
 				type: 'random',
 				containerheight: '1em'
 			});
-
-			$('#portfolio').innerfade({
-				speed: 'slow',
-				timeout: 4000,
-				type: 'sequence',
-				containerheight: '220px'
-			});
-			
-			$('.fade').innerfade({
-				speed: 'slow',
-				timeout: 1000,
-				type: 'sequence',
-				containerheight: '1.5em'
-			});
 		}
 	);
 </script>
@@ -49,22 +31,15 @@ if ($_POST)
 	<!-- Main hero unit for a primary marketing message or call to action -->
 	<div class="hero-unit">
 		<h1>Heath Info</h1>
-		<p>Vestibulum id ligula porta felis euismod semper. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
-		<p><a class="btn primary large">Learn more &raquo;</a></p>
 	</div>
 
 	<!-- Example row of columns -->
 	<div class="row">
 		<div class="span16">
 			<ul class="news-ticker" id="news">
-				<li><a href="#n1">1 Lorem ipsum dolor</a></li>
-				<li><a href="#n2">2 Sit amet, consectetuer</a></li>
-				<li><a href="#n3">3 Sdipiscing elit,</a></li>
-				<li><a href="#n4">4 sed diam nonummy nibh euismod tincidunt ut</a></li>
-				<li><a href="#n5">5 Nec Lorem.</a></li>
-				<li><a href="#n6">6 Et eget.</a></li>
-				<li><a href="#n7">7 Leo orci pede.</a></li>
-				<li><a href="#n8">8 Ratio randorus wil.</a></li>
+				<?php foreach ($data as $item):?>
+				<li><a href="berita.php?idx=<?php echo $item->idx ?>"><?php echo substr($item->judul,0,150) ?></a></li>
+				<?php endforeach?>
 			</ul>
 		</div>
 	</div>
@@ -77,14 +52,14 @@ if ($_POST)
 		<div class="span10">
 			<h2>Recent articles</h2>
 			<ul>
-				<li class="news-front"><a href="#">This is the 1st latest news item.</a></li>
-				<li class="news-front"><a href="#">This is the 2nd latest news item.</a></li>
-				<li class="news-front"><a href="#">This is the 3th latest news item.</a></li>
-				<li class="news-front"><a href="#">This is the 4th latest news item.</a></li>
-				<li class="news-front"><a href="#">This is the 5th latest news item.</a></li>
-				<li class="news-front"><a href="#">This is the 6th latest news item.</a></li>
-				<li class="news-front"><a href="#">This is the 7th latest news item.</a></li>
-				<li class="news-front"><a href="#">This is the 8th latest news item.</a></li>
+				<?php foreach ($data as $item):?>
+				<li>
+					<a href="berita.php?idx=<?php echo $item->idx ?>">
+					<b><?php echo substr($item->judul,0,150) ?></b><br />
+					<?php echo substr($item->isi,0,250).'...' ?>
+					</a>
+				</li>
+				<?php endforeach?>
 			</ul>
 		</div>
 	</div>
