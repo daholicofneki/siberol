af112282edb3f7c7702f7c73dc2f590d9c3dc08b
daholicofneki <neki.arismi@gmail.com>
2011-12-07 13:03:53 +0700

    Memisahkan top menu untuk mengetahui status yang sekarang login

---
+++ library/admin_menu.php
@@ -0,0 +1,16 @@
+<div class="topbar">
+	<div class="fill">
+		<div class="container">
+			<a class="brand" href="#"><?php echo PROJECT ?></a>
+			<form action="" method="POST" class="pull-right">
+			<?php if( empty($_SESSION['ID']) && $_SESSION['ID'] == '' ) { ?>
+				<input class="input-small" type="text" name="username" placeholder="Username">
+				<input class="input-small" type="password" name="password" placeholder="Password">
+				<button class="btn" type="submit">Sign in</button>
+			<?php } else { ?>
+				<a href="#">Welcome <b><?php echo $_SESSION['ID'] ?> ( <?php echo $_SESSION['NAME'] ?> )</b></a> | <a href="logout.php">Logout</a>
+			<?php } ?>
+			</form>
+		</div>
+	</div>
+</div>
